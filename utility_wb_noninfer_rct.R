bf6ba71ecebcfecaaada5499b5afb21e21206609
Stat-Li <36005627+Stat-Li@users.noreply.github.com>
2023-07-28 15:35:14 -0400

    Add files via upload

--- utility_wb_noninfer_rct.R
+++ utility_wb_noninfer_rct.R
@@ -19,22 +19,14 @@ prob_post = function(theta, theta_hat, theta_0, sig2_0, nevent, clt_r){
     prob = pnorm(theta, mean = theta_tilde, sd = sqrt(sig2_tilde), lower.tail = F)
     return(prob)
 }
-# prob = function(delta, a, b, m, k){
-#     pgamma(delta, shape = (a + m), rate = (b + k), lower.tail = TRUE)
-# }
-# U_total = function(t_obs, S0, t, p_shape){
-#     w_i = -log(S0)*(t_obs/t)^p_shape
-#     U = sum(w_i)
-#     return(U)
-# }
+
 pmin_stop = function(matrix_stop){
     matrix_stop = t(t(matrix_stop)*(1:dim(matrix_stop)[2]))
     matrix_stop[matrix_stop == 0] = NA
     res=do.call(pmin, c(as.data.frame(matrix_stop), list(na.rm=TRUE)))
     return(res)
 }
-# mi = 15; df = df; df_sort = df_sort; df_sort1 = df_sort1
-# ta = 4; tf = 2
+
 stage_i = function(mi, df, df_sort, df_sort1, ta, tf){
     if(dim(df_sort1)[1] < mi) {
         df$surv[df$cnsr==0] = 
@@ -62,8 +54,7 @@ stage_i = function(mi, df, df_sort, df_sort1, ta, tf){
                 duration_i = duration_i, size_i = size_i))
 }
 
-# delta_0=0.6; p_shape=1; m=20; nclt=20; ntrt=30
-# S0 = 0.53; t0 = 3; ti=c(0.5, 1); ta = 4; tf = 2
+
 optim_tune = function(delta_0, p_shape=1, m, nclt, ntrt, ti=c(0.5, 1), 
                       ta, tf, S0, t0, theta_0, sig2_0, nsim = 10000,
                       theta_noninferior){
@@ -149,7 +140,7 @@ grid_func = function(param_tune, results, ti = c(0.5, 1)){
     prob_check_super = t(t(probs) >= cutoffs_super)
     prob_check1 = prob_check_super
     prob_check1[, dim(prob_check1)[2]] = 1
-    # stop_i_super=apply(prob_check1,1, function(x) which(x!=0)[1]) # superiority stop at interim i
+    
     stop_i_super = pmin_stop(prob_check1)
     # -- futility stop -----#
     cutoffs_fut = cut_futility(ti, p_lambda = p_lambda, p_gamma = p_gamma)
@@ -157,7 +148,6 @@ grid_func = function(param_tune, results, ti = c(0.5, 1)){
     prob_check2 = prob_check_fut
     prob_check2[, dim(prob_check2)[2]] = 1
     stop_i_fut = pmin_stop(prob_check2)
-    # stop_i_fut=apply(prob_check2,1, function(x) which(x!=0)[1]) # futility stop at interim i
     
     p_type1 = mean(stop_i_super<stop_i_fut) + 
         mean((stop_i_super==stop_i_fut)*
@@ -165,7 +155,7 @@ grid_func = function(param_tune, results, ti = c(0.5, 1)){
     return(p_type1)
 }
 grid_func_fut = function(param_tune, results, ti = c(0.5, 1)){
-    # x=p_lambda; y=p_gamma
+
     p_lambda = param_tune[1]; p_gamma = param_tune[2]
     probs = results$prob
     
