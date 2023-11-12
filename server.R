c2cbaf366f5ab761657a1a0241ed8f46e45e9fa0
Stat-Li <36005627+Stat-Li@users.noreply.github.com>
2023-11-12 11:49:01 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ server <- function(input, output, session) {
   
 
   df_out <- eventReactive(input$go, {
-    
+    # show_spinner()
     design_stop <- input$design_stop
     delta0 <- input$delta0
     delta_1 <- input$delta1
@@ -14,6 +14,7 @@ server <- function(input, output, session) {
     tf <- input$tf
     surv_3year <- input$surv_3year
     x_year <- input$x_year
+    gamma_upper_bound <- input$search_gamma
     n_sims <- input$n_sims
     timing = c(as.numeric(unlist(strsplit(input$interim_timing," "))), 1)
     results = NULL
@@ -29,7 +30,8 @@ server <- function(input, output, session) {
       n_max=ceiling(m_target/(1-1/ta*integrate(ff,tf,ta+tf)$value))
       for (h in 1:length(pram_gamma_a)) {
         
-        pram_gamma_b = 2*pram_gamma_a[h]/(1 + delta_1)
+        # pram_gamma_b = 2*pram_gamma_a[h]/(1 + delta_1)
+        pram_gamma_b = 2*pram_gamma_a[h]/(1 + delta0)
         
         results_h0 = optim_tune(delta_0 = delta0, m = m_target, n=n_max, ti=timing, nsim = n_sims,
                                 pram_gamma_a = pram_gamma_a[h], pram_gamma_b = pram_gamma_b,
@@ -38,7 +40,7 @@ server <- function(input, output, session) {
         
         
         p1 = seq (0.01, 0.99, by = 0.01)
-        p2 = seq (0.01, 3, by = 0.01)
+        p2 = seq (0.01, gamma_upper_bound, by = 0.01)
         
         p.tune = expand.grid(x = p1, y = p2)
         if(input$design_stop == "Futility and superiority stopping"){
@@ -66,9 +68,16 @@ server <- function(input, output, session) {
           grid_search_all = round(grid_search_all, digits = 3)
           p_opt = sum_1[which.min(grid_search_all[,5]),]
           p_opt = as.numeric(p_opt)
-          p_cut_fut = cut_futility(timing, p_lambda = p_opt[1], p_gamma = p_opt[2])
-          p_cut_sup = cut_super(timing, p_lambda = p_opt[1])
-          results_stop = rbind(results_stop, cbind(p_cut_fut, p_cut_sup))
+          # p_cut_fut = cut_futility(timing, p_lambda = p_opt[1], p_gamma = p_opt[2])
+          # p_cut_sup = cut_super(timing, p_lambda = p_opt[1])
+          # results_stop = rbind(results_stop, cbind(p_cut_fut, p_cut_sup))
+          tot_bound_fut = tot_stop_futility(gamma_a = pram_gamma_a[h], gamma_b = pram_gamma_b, 
+                                        p_lambda = p_opt[1], p_gamma = p_opt[2], 
+                                        t_interim = timing, event_tot = m_target)
+          tot_bound_sup = tot_stop_superiority(gamma_a = pram_gamma_a[h], gamma_b = pram_gamma_b, 
+                                            p_lambda = p_opt[1], 
+                                            t_interim = timing, event_tot = m_target)
+          results_stop = rbind(results_stop, cbind(tot_bound_fut, tot_bound_sup))
           
           PRN0 = grid_search_all[which.min(grid_search_all[,5]),1]
           PET0 = grid_search_all[which.min(grid_search_all[,5]),2]
@@ -97,9 +106,11 @@ server <- function(input, output, session) {
           results = rbind(results, xx1, xx2)
         }
         if(input$design_stop == "Futility stopping only"){
+          source("stopping_boundary.R")
           grid_search = apply(p.tune, 1, grid_func_fut, results = results_h0, ti = timing)
           grid_search = round(grid_search, digits = 3)
           sum_0=p.tune[which(abs(grid_search-type1) < 0.01 & grid_search < type1), ]
+          # dim(sum_0)
           
           results_h1 = optim_tune(delta_0 = delta_1, m = m_target, n=n_max, ti=timing, nsim = n_sims,
                                   pram_gamma_a = pram_gamma_a[h], pram_gamma_b = pram_gamma_b,
@@ -120,9 +131,12 @@ server <- function(input, output, session) {
           grid_search_all = round(grid_search_all, digits = 3)
           p_opt = sum_1[which.min(grid_search_all[,3]),]
           p_opt = as.numeric(p_opt)
-          p_cut_fut = cut_futility(timing, p_lambda = p_opt[1], p_gamma = p_opt[2])
-          
-          results_stop = rbind(results_stop, cbind(p_cut_fut))
+          # p_cut_fut = cut_futility(timing, p_lambda = p_opt[1], p_gamma = p_opt[2])
+          # results_stop = rbind(results_stop, cbind(p_cut_fut))
+          tot_bound = tot_stop_futility(gamma_a = pram_gamma_a[h], gamma_b = pram_gamma_b, 
+                               p_lambda = p_opt[1], p_gamma = p_opt[2], 
+                               t_interim = timing, event_tot = m_target)
+          results_stop = rbind(results_stop, tot_bound)
           
           PRN0 = grid_search_all[which.min(grid_search_all[,3]),1]
           PET0 = grid_search_all[which.min(grid_search_all[,3]),2]
@@ -160,7 +174,7 @@ server <- function(input, output, session) {
                                          rep(timing, length(pram_gamma_a)),
                                          results_stop))
         names(df_final_stop) = c('Design', 'Gamma prior shape',
-                                 'Information time', 'Futility stopping if posterior prob. <')
+                                 'Information time', 'Stop trial if TOT (transformed) <')
       }
       if(input$design_stop == "Futility and superiority stopping"){
         df_final_ops = data.frame(cbind(rep(1:length(pram_gamma_a), each = 2), weibull_shape,
@@ -175,14 +189,15 @@ server <- function(input, output, session) {
                                          rep(timing, length(pram_gamma_a)),
                                          results_stop))
         names(df_final_stop) = c('Design', 'Gamma prior shape',
-                                 'Information time', 'Futility stopping if posterior prob. <', 
-                                 'Superiority stopping if posterior prob. >=')
+                                 'Information time', 'Stop for futility if TOT (transformed) <', 
+                                 'Stop for superiority if TOT (transformed) >=')
       }
     }
     if(input$design_arm=="Two-arm RCT"){
       source("utility_wb_noninfer_rct.R")
       eta = -log(delta0)
-      theta0 = -0.5*log(delta_1)
+      # theta0 = -0.5*log(delta_1)
+      theta0 = -log(delta0)
       sigma2_0 = as.numeric(unlist(strsplit(input$sigma2_0," ")))
       clt_ratio = input$ss_ratio/(input$ss_ratio + 1)
       m_target = ceiling((qnorm(1-type1)+qnorm(1-type2))^2/(log(delta_1)+eta)^2/clt_ratio/(1 - clt_ratio))
@@ -197,7 +212,7 @@ server <- function(input, output, session) {
                                 ta = ta, tf = tf, theta_0 = theta0, sig2_0 = sigma2_0[h], nsim = n_sims,
                                 theta_noninferior = eta)
         p1 = seq (0.01, 0.99, by = 0.01)
-        p2 = seq (0.01, 3, by = 0.01)
+        p2 = seq (0.01, gamma_upper_bound, by = 0.01)
         p.tune = expand.grid(x = p1, y = p2)
         if(input$design_stop == "Futility and superiority stopping"){
           grid_search = apply(p.tune, 1, grid_func, results = results_h0, ti = timing)
@@ -222,7 +237,7 @@ server <- function(input, output, session) {
           p_cut_fut = cut_futility(timing, p_lambda = p_opt[1], p_gamma = p_opt[2])
           p_cut_sup = cut_super(timing, p_lambda = p_opt[1])
           results_stop = rbind(results_stop, cbind(p_cut_fut, p_cut_sup))
-          
+          # results_tune = rbind(results_tune, c(weibull_shape, sigma2_0[h],  p_opt))
           PRN0 = grid_search_all[which.min(grid_search_all[,5]),1]
           PET0 = grid_search_all[which.min(grid_search_all[,5]),2]
           PET_EFS0 = grid_search_all[which.min(grid_search_all[,5]),3]
@@ -274,9 +289,9 @@ server <- function(input, output, session) {
           p_opt = sum_1[which.min(grid_search_all[,3]),]
           p_opt = as.numeric(p_opt)
           p_cut_fut = cut_futility(timing, p_lambda = p_opt[1], p_gamma = p_opt[2])
-          
+          # p_cut_sup = cut_super(timing, p_lambda = p_opt[1])
           results_stop = rbind(results_stop, cbind(p_cut_fut))
-          
+          # results_tune = rbind(results_tune, c(weibull_shape, sigma2_0[h],  p_opt))
           PRN0 = grid_search_all[which.min(grid_search_all[,3]),1]
           PET0 = grid_search_all[which.min(grid_search_all[,3]),2]
           size0 = grid_search_all[which.min(grid_search_all[,3]),3]
@@ -312,7 +327,7 @@ server <- function(input, output, session) {
                                          rep(timing, length(sigma2_0)),
                                          results_stop))
         names(df_final_stop) = c('Design', 'Normal prior variance',
-                                 'Information time', 'Futility stopping if posterior prob. <')
+                                 'Information time', 'Stop trial if TOT (transformed) <=')
       }
       if(input$design_stop == "Futility and superiority stopping"){
         df_final_ops = data.frame(cbind(rep(1:length(sigma2_0), each = 2), weibull_shape,
@@ -332,6 +347,9 @@ server <- function(input, output, session) {
       }
     }
     
+    # zz=data.frame(cbind(n_max, clt_ratio))
+    # names(zz)=c('x1 (%)','PET_f')
+    # return(zz)
     return(list(df_final_ops, df_final_stop))
 
   })
